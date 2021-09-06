4127c58b815384774ab814331a980c7064d71d80
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-05 22:59:14 -0300

    página home desenvolvida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import { FiCalendar, FiUser } from "react-icons/fi";
 
 import { getPrismicClient } from '../services/prismic';
 
+import Logo from '../assets/Logo.svg'
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -27,7 +30,46 @@ interface HomeProps {
 
 export default function Home() {
   return (
-    <h1>Oi</h1>
+    <div className={styles.PostContainer}>
+      <Image src={Logo} alt="Logo" width="100%" height="100%" />      
+      <div className={styles.post}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.Icons}>
+          <FiCalendar/><time>19 abr 2021</time>
+          <FiUser/><span>Gabriel Merigo</span>
+        </div>
+      </div>
+
+      <div className={styles.post}>
+        <h1>Criando um app CRA do zero</h1>
+        <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App.</p>
+        <div className={styles.Icons}>
+          <FiCalendar/><time>19 Abr 2021</time>
+          <FiUser/><span>Danilo Vieira</span>
+        </div>
+      </div>
+
+      <div className={styles.post}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.Icons}>
+          <FiCalendar/><time>19 abr 2021</time>
+          <FiUser/><span>Gabriel Merigo</span>
+        </div>
+      </div>
+
+      <div className={styles.post}>
+        <h1>Criando um app CRA do zero</h1>
+        <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App.</p>
+        <div className={styles.Icons}>
+          <FiCalendar/><time>19 Abr 2021</time>
+          <FiUser/><span>Danilo Vieira</span>
+        </div>
+      </div>
+
+      <a>Carregar mais posts</a>
+    </div>
   )
 }
 
