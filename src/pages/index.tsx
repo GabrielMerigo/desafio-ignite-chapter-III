d9200341c5a149c294221b9b87e1683bdff0df23
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-13 19:34:26 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import commonStyles from '../styles/common.module.scss';
 import { FiCalendar, FiUser } from "react-icons/fi";
 import styles from './home.module.scss';
 import Header from '../components/Header'
+import { Head } from 'next/document';
 
 
 interface Post {
   uid?: string;
   first_publication_date: string | null;
   data: {
+    banner: string;
     title: string;
     subtitle: string;
     author: string;
@@ -19,64 +21,74 @@ interface Post {
 
 interface PostPagination {
   next_page: string;
-  results: Posts;
+  results: Post[];
 }
 
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-interface Posts {
-  posts: Post[]
-}
+
 
 export default function Home(props: HomeProps): JSX.Element{
-  console.log(props)
-  setTimeout(() => {
-    console.log(props.postsPagination.results)
-  }, 1000)
+  // console.log(props.postsPagination.results)
   return (
-    <div className={styles.PostContainer}>
-      {/* <Header />
-      {(props.postsPagination.results.posts.map(post => {
-        <div key={post.uid} className={styles.post}>
-          <h1>{post.data.title}</h1>
-          <p>{post.data.subtitle}</p>
-          <div className={styles.Icons}>
-            <FiCalendar /><time>19 abr 2021</time>
-            <FiUser /><span>Gabriel Merigo</span>
+    <>
+      <div className={styles.PostContainer}>
+        <Header />
+        {(props.postsPagination.results.map(post => {
+          <div key={post.uid} className={styles.post}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
+            <div className={styles.Icons}>
+              <FiCalendar /><time>19 abr 2021</time>
+              <FiUser /><span>Gabriel Merigo</span>
+            </div>
           </div>
-        </div>
-      }))} */}
+        }))}
 
-      <a>Carregar mais posts</a>
-    </div>
+        <a>Carregar mais posts</a>
+      </div>
+    </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<HomeProps> = async ({
+  preview = false,
+  previewData,
+}) => {
   const prismic = getPrismicClient();
+
   const postsResponse = await prismic.query(
-    [Prismic.Predicates.at('document.type', 'post')],
-    { fetch: ['post.title', 'post.subtitle', 'post.author'], pageSize: 5 }
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.banner', 'post.title', 'post.subtitle', 'post.author'],
+      pageSize: 2, // default 20
+      // ref: previewData?.ref ?? null,
+    }
   );
 
   const posts = postsResponse.results.map(post => {
     return {
       uid: post.uid,
-      first_publication_date: post.first_publication_date,
       data: {
+        banner: post.data.banner.url,
         title: post.data.title,
         subtitle: post.data.subtitle,
         author: post.data.author,
-      }
-    }
-  })
+      },
+      first_publication_date: post.first_publication_date,
+    };
+  });
 
-  console.log(postsResponse.results)
   return {
     props: {
-      posts
-    }
-  }
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results: posts,
+      },
+      preview,
+    },
+  };
 };
